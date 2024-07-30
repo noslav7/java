18c8eafe0d6b860910fe831769575eb957b19f01
noslav7 <noslav7@gmail.com>
2024-07-30 12:01:15 +0500

    PUPPET-406 - добавление зависимостей в pom.xml - настройка подключения к puppet_db - приведение класса User в соответствие с требованиями задания - создание JPA-репозитория для сущности User

--- src/main/java/com/aston/frontendpracticeservice/domain/entity/User.java
+++ src/main/java/com/aston/frontendpracticeservice/domain/entity/User.java
@@ -1,19 +1,48 @@
 package com.aston.frontendpracticeservice.domain.entity;
 
-import com.aston.frontendpracticeservice.security.Role;
-import lombok.Builder;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Set;
+import java.time.LocalDate;
 
 @Getter
 @Setter
-@Builder
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Past
+    private LocalDate dateOfBirth;
+
+    @NotBlank
+    @Size(min = 12, max = 12)
+    private String inn;
+
+    @NotBlank
+    @Size(min = 11, max = 11)
+    private String snils;
+
+    @NotBlank
+    @Size(min = 10, max = 10)
+    private String passportNumber;
+
+    @NotBlank
     private String login;
 
+    @NotBlank
+    @Size(min = 8)
     private String password;
-
-    private Set<Role> roles;
 }
---
+++ src/main/java/com/aston/frontendpracticeservice/repository/UserRepositoryJPA.java
@@ -0,0 +1,12 @@
+package com.aston.frontendpracticeservice.repository;
+
+import com.aston.frontendpracticeservice.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepositoryJPA extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
+}
